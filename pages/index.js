0668bfdf67eef3c622b7a7136e84d89f7113364d
Alex <alexcsalesdev@gmail.com>
2022-11-12 12:32:44 -0300

    implementado modo dark e switch.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
@@ -11,7 +10,6 @@ function HomePage() {
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -30,6 +28,8 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
@@ -39,7 +39,7 @@ const StyledHeader = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
-    padding: 25px 32px 0px 32px;
+    padding: 16px 32px;
     gap: 16px;
   }
   `;
--- src/components/Menu/components/Search.js
+++ src/components/Menu/components/Search.js
@@ -13,7 +13,9 @@ const StyledSearch = styled.div`
     width: 80%;
     padding: 4px 6px;
     border: 1px solid ${({ theme }) => theme.borderBase};
-    border-radius: 40px 0 0 40px;    
+    border-radius: 40px 0 0 40px;
+    font-size: 1rem;
+    padding-left:1rem;
 
     outline: none;
     color: ${({ theme }) => theme.textColorBase};
@@ -39,11 +41,6 @@ const StyledSearch = styled.div`
   }
 `;
 
-// Home 
-// Menu
-// Search
-// Informação sempre desce
-
 export default function Search({ valorDoFiltro, setValorDoFiltro }) {
   
   const valorDaBusca = valorDoFiltro;
@@ -51,7 +48,7 @@ export default function Search({ valorDoFiltro, setValorDoFiltro }) {
 
   return (
     <StyledSearch>
-      <input type="text" onChange={(e) => setValorDaBusca(e.target.value)} value={valorDaBusca} />
+      <input type="text" onChange={(e) => setValorDaBusca(e.target.value)} value={valorDaBusca} placeholder="Pesquisar"/>
       <button>
       <svg aria-hidden="true" class="s-input-icon s-input-icon__search svg-icon iconSearch" width="18" height="18" viewBox="0 0 18 18"><path d="m18 16.5-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5ZM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0Z"></path></svg>
       </button>
