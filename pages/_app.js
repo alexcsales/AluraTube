89f7261593b36a9e064b8eb895c76c6fddce1642
Alex <alexcsalesdev@gmail.com>
2022-11-13 12:34:33 -0300

    Inplementado: formulario de cadastro

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import ColorModeProvider, {ColorModeContext} from "../src/components/Menu/components/ColorMode";
+import RegisterVideo from "../src/components/Menu/components/RegisterVideo";
+
 const theme = {
     light: {
         backgroundBase: "#f9f9f9",
@@ -27,13 +29,14 @@ function ProviderWrapper(props) {
     )
 }
 
-function MyApp({ Component, pageProps }) {
+function Root({ Component, pageProps }) {
     const contexto = React.useContext(ColorModeContext);
     console.log(contexto.mode);
     return (
         <ThemeProvider theme={theme[contexto.mode]}>
             <CSSReset />
             <Component {...pageProps} />
+            <RegisterVideo />
         </ThemeProvider>
     )
 }
@@ -41,7 +44,7 @@ function MyApp({ Component, pageProps }) {
 export default function _App(props) {
     return (
         <ProviderWrapper>
-            <MyApp {...props} />
+            <Root {...props} />
         </ProviderWrapper>
     )
 };
